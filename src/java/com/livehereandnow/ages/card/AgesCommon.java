a892d31f8b0bccd25f0f54710e80aaff16dc9afa
Mark Chenpingling <mark@livehereandnow.com>
2014-06-01 10:19:36 +0800

    refactor  AgesCardShowStyle to AgesCommon add String[] AGE_NAME to it

---
+++ src/java/com/livehereandnow/ages/card/AgesCommon.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.livehereandnow.ages.card;
+
+/**
+ *
+ * @author mark
+ */
+public interface AgesCommon {
+
+    int STYLE_政府區 = 100;
+    int STYLE_領袖區 = 101;
+    String[] AGE_NAME = {"\uFF21", "\uFF29", "\uFF29\uFF29", "\uFF29\uFF29\uFF29"};//http://www.unicode.org/charts/PDF/UFF00.pdf
+
+}
